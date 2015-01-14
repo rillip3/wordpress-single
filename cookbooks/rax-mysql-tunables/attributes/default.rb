2f7322c73c1eadaa86669ce87c7c1a52b3fd29a9
Philip Eatherington <philip.eatherington@rackspace.com>
2015-01-14 12:44:19 -0600

    update for vend

--- cookbooks/rax-mysql-tunables/attributes/default.rb
+++ cookbooks/rax-mysql-tunables/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:rax_mysql_tunables][:config_options][:mysqld]['innodb-buffer-pool-size'] = "#{(node['memory']['total'].to_i * 0.6 ).floor * 1024}"
+default[:rax_mysql_tunables][:config_options][:mysqld]['socket'] = '/var/run/mysqld/mysqld.sock'
